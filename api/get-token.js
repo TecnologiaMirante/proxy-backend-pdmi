2bc110a69b1e62842c1159daa3b7c10d62ab2349
VictorEmanuel08 <victorem3181@gmail.com>
2025-01-29 10:33:54 -0300

    definindo dominio de origem especifico

--- api/get-token.js
+++ api/get-token.js
@@ -2,6 +2,14 @@ const axios = require("axios");
 const qs = require("qs");
 
 module.exports = async function (req, res) {
+  // Adicione o cabeçalho CORS
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://pdmi-mirante.flutterflow.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método não permitido" });
   }
@@ -9,7 +17,7 @@ module.exports = async function (req, res) {
   try {
     const data = qs.stringify(req.body);
 
-    //POST
+    // POST
     const response = await axios.post(
       "https://login.microsoftonline.com/1e6a5fc4-072c-4f6e-bd38-3d312e331366/oauth2/v2.0/token",
       data,
